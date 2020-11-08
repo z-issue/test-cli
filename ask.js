b37797e8c77fba81636016a00d04bbd5eef117fe
ZhouPan <darkadapted66@163.com>
2020-11-08 18:40:51 +0800

    create ask file

---
+++ ask.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        name: 'version',
+        type: 'input',
+        message: 'please input project version',
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'please input project description',
+    },
+]
